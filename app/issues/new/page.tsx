6b83755ec7ca07f588c6312f04ee98664b4f3dd7
Tonny Oseko <tonnyoseko0@gmail.com>
2023-10-18 00:19:00 +0300

    customize radix ui theme

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,23 +1,27 @@
 "use client";
+import { Button, Flex, Heading, TextArea, TextField } from "@radix-ui/themes";
 import React, { useState } from "react";
 
 const NewIssue = () => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   return (
-    <form>
-      <input name="title" className="" placeholder="Title" value={title}>
-        Title
-      </input>
-      <textarea
-        name="description"
-        placeholder="Description"
-        value={description}
-      >
-        Description
-      </textarea>
-      <button type="submit" className="bg-blue p-4 text-white"></button>
-    </form>
+    <div>
+      <Heading mb="2" size="4">
+        Add Issue
+      </Heading>
+      <Flex direction="column" gap="3" style={{ maxWidth: 500 }}>
+        <TextField.Root>
+          <TextField.Input placeholder="Title" name="title" />
+        </TextField.Root>
+        <TextArea
+          variant="classic"
+          placeholder="Description"
+          name="description"
+        />
+        <Button>submit</Button>
+      </Flex>
+    </div>
   );
 };
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import "@radix-ui/themes/styles.css";
+import "./theme-config.css";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Theme } from "@radix-ui/themes";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 import Navbar from "./Navbar";
 
 const inter = Inter({
   subsets: ["latin"],
+  variable: "--font-inter",
 });
 
 export const metadata: Metadata = {
@@ -21,10 +23,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Theme>
+      <body className={inter.variable}>
+        <Theme appearance="light" accentColor="violet">
           <Navbar />
-          <main>{children}</main>
+          <main className="p-5">{children}</main>
         </Theme>
       </body>
     </html>
